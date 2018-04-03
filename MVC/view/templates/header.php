37ab3024cfcddea41930d859c6bbebaeba442cf2
Rens Gouman <99030922@mydavinci.nl>
2018-04-03 09:41:30 +0200

    start with hospital

---
+++ MVC/view/templates/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>hospital</title>	
+	<link rel="stylesheet" href="<?= URL ?>">
+	<meta charset="utf-8">
+</head>
+<body>
+	<nav>
+	<ul>
+		<li><a href="<?= URL ?>home/index">Home</a></li>
+		<li><a href="<?= URL ?>student/index">Students</a></li>
+		<li><a href="<?= URL ?>hospital/index">hospital</a></li>
+	</ul>
+	</nav>
